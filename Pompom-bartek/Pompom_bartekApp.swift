fda2c1f770bfd5b7de2904030d1f2f9b57da1e06
BartoszKut <bartosz.kut.programista@gmail.com>
2021-05-23 22:07:08 +0200

    Initial Commit

---
+++ Pompom-bartek/Pompom_bartekApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pompom_bartekApp.swift
+//  Pompom-bartek
+//
+//  Created by Bartosz Kut on 23/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Pompom_bartekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
